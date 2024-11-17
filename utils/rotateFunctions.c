5394fb200d7d145ccf1d13565ad6f00544659572
Arthur Legrix <alegrix@c2r11p4.42lehavre.fr>
2024-11-18 00:57:02 +0100

    rotate v2

--- utils/rotateFunctions.c
+++ utils/rotateFunctions.c
@@ -6,11 +6,11 @@
 /*   By: alegrix <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 00:24:28 by alegrix           #+#    #+#             */
-/*   Updated: 2024/11/18 00:47:24 by alegrix          ###   ########.fr       */
+/*   Updated: 2024/11/18 00:56:16 by alegrix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	rotate(t_list	**lst)
+void	rotate(t_list **lst)
 {
 	t_list	*new;
 
@@ -19,4 +19,10 @@ void	rotate(t_list	**lst)
 	new->content = *lst->content;
 	ft_delone(*lst, free);
 	ft_addback(lst, new);
+}
+
+void	double_rotate(t_list **first, t_list **second)
+{
+	rotate(first);
+	rotate(second);
 }
