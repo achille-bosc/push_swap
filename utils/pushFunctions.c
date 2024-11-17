a2639b03e35f652df7705710aa2aaffd2b284e56
Arthur Legrix <alegrix@c2r11p4.42lehavre.fr>
2024-11-18 00:20:16 +0100

    pushfunction v1

--- utils/pushFunctions.c
+++ utils/pushFunctions.c
@@ -6,8 +6,17 @@
 /*   By: abosc <abosc@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/17 23:33:56 by abosc             #+#    #+#             */
-/*   Updated: 2024/11/17 23:48:02 by alegrix          ###   ########.fr       */
+/*   Updated: 2024/11/18 00:15:02 by alegrix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	push(t_list **first)
+#include "push_swap.h"
+
+void	push(t_list **first, t_list **second)
+{
+	t_list	*new_m;
+
+	new_m = *first;
+	ft_delone(*first, free);
+	ft_lstadd_front(second, new_m);
+}
