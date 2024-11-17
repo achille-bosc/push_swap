e4a652593820520f787a413a0c95c668a8f400df
Arthur Legrix <alegrix@c2r11p4.42lehavre.fr>
2024-11-18 00:48:13 +0100

    rotate v1

--- utils/pushFunctions.c
+++ utils/pushFunctions.c
@@ -6,7 +6,7 @@
 /*   By: abosc <abosc@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/17 23:33:56 by abosc             #+#    #+#             */
-/*   Updated: 2024/11/18 00:15:02 by alegrix          ###   ########.fr       */
+/*   Updated: 2024/11/18 00:38:45 by alegrix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	push(t_list **first, t_list **second)
 {
 	t_list	*new_m;
 
-	new_m = *first;
+	new_m->content = *first->content;
 	ft_delone(*first, free);
 	ft_lstadd_front(second, new_m);
 }
--- utils/rotateFunctions.c
+++ utils/rotateFunctions.c
@@ -6,8 +6,17 @@
 /*   By: alegrix <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 00:24:28 by alegrix           #+#    #+#             */
-/*   Updated: 2024/11/18 00:24:30 by alegrix          ###   ########.fr       */
+/*   Updated: 2024/11/18 00:47:24 by alegrix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+void	rotate(t_list	**lst)
+{
+	t_list	*new;
 
+	if (*lst->content == NULL || *lst->next->content == NULL)
+		return ;
+	new->content = *lst->content;
+	ft_delone(*lst, free);
+	ft_addback(lst, new);
+}
