a5e6cb037826fa25adef2c3e4c1b512ace9263fd
Achille Bosc <abosc@c2r11p3.42lehavre.fr>
2024-11-18 01:36:05 +0100

    revRotate double

--- utils/revRotateFunctions.c
+++ utils/revRotateFunctions.c
@@ -6,17 +6,23 @@
 /*   By: abosc <abosc@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 00:24:41 by alegrix           #+#    #+#             */
-/*   Updated: 2024/11/18 01:32:08 by abosc            ###   ########.fr       */
+/*   Updated: 2024/11/18 01:36:00 by abosc            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-void	reverse_rotate_a(t_list **lst)
+void	reverse_rotate(t_list **lst)
 {
 	t_list	*new;
 	
 	new->content = ft_lstlast(*lst)->content;
 	ft_lstadd_front(lst, new);
 	ft_lstdelone(ft_lstlast(*lst), free);
+}
+
+void	double_rr(t_list **lst_a, t_list **lst_b)
+{
+	reverse_rotate(lst_a);
+	reverse_rotate(lst_b);
 }
