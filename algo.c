63ef88fa499ee4aa65af006fd6f38e7a53e716e6
Arthur Legrix <alegrix@c2r11p4.42lehavre.fr>
2024-11-18 02:26:35 +0100

    1st algo

--- algo.c
+++ algo.c
@@ -6,8 +6,42 @@
 /*   By: alegrix <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 01:16:05 by alegrix           #+#    #+#             */
-/*   Updated: 2024/11/18 01:19:50 by alegrix          ###   ########.fr       */
+/*   Updated: 2024/11/18 02:25:56 by alegrix          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.h"
 
+void	algo(t_list **a, t_list **b)
+{
+	while (ft_lstsize(*a) > 1)
+	{
+		if (fst_sup_sec(*a) == 1)
+		{
+			ft_printf("sa");
+			swap_a(a);
+		}
+		ft_printf("pb");
+		push_b(a, b);
+		if (fst_sup_sec(*b) == 0)
+		{
+			ft_printf("sb\n");
+			swap_b(b);
+		}
+	}
+	while (ft_lstsize(*b) > 0)
+	{
+		if (fst_sup_sec(*b) == 0)
+		{
+			ft_printf("sb");
+			swap_a(b);
+		}
+		ft_printf("pa");
+		push_b(b, a);
+		if (fst_sup_sec(*a) == 1)
+		{
+			ft_printf("sb\n");
+			swap_b(a);
+		}
+	}
+}
