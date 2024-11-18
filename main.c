25b6d185ff2293a7d905bcb0b752f431b6e29591
Achille Bosc <abosc@c2r11p3.42lehavre.fr>
2024-11-18 02:34:38 +0100

    achille pas la

--- main.c
+++ main.c
@@ -1,32 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   swapFunctions.c                                    :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abosc <abosc@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/17 23:24:04 by abosc             #+#    #+#             */
-/*   Updated: 2024/11/18 02:15:01 by abosc            ###   ########.fr       */
+/*   Created: 2024/11/18 01:43:12 by abosc             #+#    #+#             */
+/*   Updated: 2024/11/18 02:07:46 by abosc            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../push_swap.h"
-
-void	swap(t_list **lst, char col)
+int	is_error_args(int argc, char **argv)
 {
-	t_list	*new;
-
-	new = ft_lstnew((*lst)->content);
-	ft_lstdelone(lst, free);
-	new->next = (*lst)->next;
-	(*lst)->next = new;
-	if (col == 'a')
-		
+	if (argc < 2)
+		return (0);
+	
 }
 
-void	double_swap(t_list **lst_a, t_list **lst_b)
+int	main(int argc, char **argv)
 {
-	swap_a(lst_a);
-	swap_b(lst_b);
-	ft_printf("ss");
+	if (is_error_args(argc, argv))
+		return (0);
 }
