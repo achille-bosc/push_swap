65def32072a335bc9c6e85477f64fc485b1579aa
Achille Bosc <abosc@c1r2p3.42lehavre.fr>
2024-11-18 22:24:56 +0100

    add functions in header

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: abosc <abosc@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/17 23:37:36 by abosc             #+#    #+#             */
-/*   Updated: 2024/11/18 22:20:09 by abosc            ###   ########.fr       */
+/*   Updated: 2024/11/18 22:24:47 by abosc            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,13 @@
 # include "libft/libft.h"
 
 void	algo(t_list **a, t_list **b);
-
+int		fst_sup_sec(t_list *lst);
+int		sec_sup_thd(t_list *lst);
+void	push(t_list **first, t_list **second, char x);
+void	reverse_rotate(t_list **lst, char x);
+void	double_rr(t_list **lst_a, t_list **lst_b);
+void	rotate(t_list **lst, char x);
+void	double_rotate(t_list **first, t_list **second);
+void	swap(t_list **lst, char x);
+void	double_swap(t_list **lst_a, t_list **lst_b);
 #endif
