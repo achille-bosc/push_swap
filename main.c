42634cdc301ba9e46f66dafd9fa23265c01075e6
Achille Bosc <abosc@c1r2p3.42lehavre.fr>
2024-11-18 22:21:22 +0100

    first main without checks

--- main.c
+++ main.c
@@ -3,12 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: achillebosc <achillebosc@student.42.fr>    +#+  +:+       +#+        */
+/*   By: abosc <abosc@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 01:43:12 by abosc             #+#    #+#             */
-/*   Updated: 2024/11/18 16:47:52 by achillebosc      ###   ########.fr       */
+/*   Updated: 2024/11/18 22:20:51 by abosc            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "push_swap.h"
 
 int	is_error_one_args(int argc, char **argv)
 {
@@ -25,10 +27,24 @@ int	is_error_few_args(int argc, char **argv)
 
 int	main(int argc, char **argv)
 {
-	if (argc == 2)
-		if (is_error_one_args(argc, argv))
-			return (0);
-	else
-		if (is_error_few_args(argc, argv))
-			return (0);
+	// if (argc == 2)
+	// 	if (is_error_one_args(argc, argv))
+	// 		return (0);
+	// else
+	// 	if (is_error_few_args(argc, argv))
+	// 		return (0);
+	t_list	*lst_a;
+	t_list	*lst_b;
+	t_list	*temps;
+	lst_a = ft_lstnew(argv[1]);
+	int i;
+	i = 2;
+	while (i < argc)
+	{
+		temps = ft_lstnew(argv[i]);
+		lst_a->next = temps;
+		lst_a = temps;
+		i++;
+	}
+	algo(lst_a, lst_a);
 }
